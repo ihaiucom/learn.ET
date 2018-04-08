f9d9b5b1e97c75b46d4cce6de452eb6cecf2db98
ihaiu <ihaiu@qq.com>
2018-04-08 23:27:23 +0800

    no message

--- learnET3.2/ETServer/Server/Model/Base/Helper/DllHelper.cs
+++ learnET3.2/ETServer/Server/Model/Base/Helper/DllHelper.cs
@@ -9,11 +9,11 @@ namespace ETModel
 	{
 		public static Assembly GetHotfixAssembly()
 		{
-			byte[] dllBytes = File.ReadAllBytes("./Hotfix.dll");
+            byte[] dllBytes = File.ReadAllBytes("../../Bin/Hotfix.dll");
 #if __MonoCS__
-			byte[] pdbBytes = File.ReadAllBytes("./Hotfix.dll.mdb");
+            byte[] pdbBytes = File.ReadAllBytes("../../Bin/Hotfix.dll.mdb");
 #else
-			byte[] pdbBytes = File.ReadAllBytes("./Hotfix.pdb");
+            byte[] pdbBytes = File.ReadAllBytes("../../Bin/Hotfix.pdb");
 #endif
 			Assembly assembly = Assembly.Load(dllBytes, pdbBytes);
 			return assembly;
